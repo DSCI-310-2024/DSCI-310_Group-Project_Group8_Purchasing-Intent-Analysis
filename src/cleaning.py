fbf2f9162dc0f6cd8e61ae81448923ac3ac8d17b
Sai Pusuluri <pusulurs@gmail.com>
2024-03-14 20:54:07 -0700

    added echo statements for better observability

--- src/cleaning.py
+++ src/cleaning.py
@@ -11,18 +11,28 @@ def process_data(input_file, processed_file):
     # Data cleaning/pre-processing steps go here
     # drop null
     if (data.isnull().sum() > 0):
+        null_values = data.isnull().sum()
+        click.echo(f"Null values before cleaning:\n{null_values}")
         data.dropna()
+        click.echo(f"Null values after cleaning:\n{null_values}")
 
     # drop duplicates
     if (data.duplicated().value_counts() > 0):
+        duplicates = data[data.duplicated()]
+        click.echo(f"Duplicates before cleaning:\n{duplicates}")
 
         # drop duplicates
         data.drop_duplicates(inplace=True)
+        click.echo(f"Duplicates after cleaning: \n{duplicates}")
 
         # Drop "OperatingSystems" column as mentioned in ipynb file
         data.drop(columns = ["OperatingSystems"])
+        click.echo(f"dropped OperatingSystems Column")
+
     else :
+        click.echo(f"No duplicates found!")
         data.drop(columns = ["OperatingSystems"])
+        click.echo(f"dropped OperatingSystems Column")
 
     # Save the processed data to a new CSV file
     data.to_csv(processed_file, index=False)
