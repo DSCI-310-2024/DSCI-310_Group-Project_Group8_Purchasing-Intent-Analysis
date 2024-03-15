a3b5c5dad8243794489b6f8635c76b595d11691d
Sai Pusuluri <pusulurs@gmail.com>
2024-03-14 20:47:43 -0700

    added drop null and else branch to cleaning script

--- src/cleaning.py
+++ src/cleaning.py
@@ -9,19 +9,25 @@ def process_data(input_file, processed_file):
     data = pd.read_csv(input_file)
     
     # Data cleaning/pre-processing steps go here
+    # drop null
+    if (data.isnull().sum() > 0):
+        data.dropna()
+
     # drop duplicates
     if (data.duplicated().value_counts() > 0):
 
         # drop duplicates
         data.drop_duplicates(inplace=True)
-        
+
         # Drop "OperatingSystems" column as mentioned in ipynb file
+        data.drop(columns = ["OperatingSystems"])
+    else :
         data.drop(columns = ["OperatingSystems"])
 
     # Save the processed data to a new CSV file
     data.to_csv(processed_file, index=False)
     
-    click.echo("Data processed and saved successfully to {}".format(processed_file))
+    click.echo("Data cleaned and saved successfully to {}".format(processed_file))
 
 if __name__ == '__main__':
     process_data()
