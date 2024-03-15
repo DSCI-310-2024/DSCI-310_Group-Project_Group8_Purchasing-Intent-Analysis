7380512abb7d8c6713a0609ba13ffcc7e7a725d8
Nour Shawky <nourshawky03@hotmail.com>
2024-03-14 18:16:05 -0700

    fixes in raw_data.py file

--- src/raw_data.py
+++ src/raw_data.py
@@ -2,8 +2,8 @@ import pandas as pd
 import click
 
 @click.command()
-@click.argument('input_url', type = 'str')
-@click.argument('output_path', type = 'str')
+@click.argument('input_url', type = str)
+@click.argument('output_path', type = str)
 def read_data(input_url, output_path): 
     #read in data from input URL
     data = pd.read_csv(input_url)
