e52581a233960f617483bc8a1d193faed51082b5
Nour Shawky <nourshawky03@hotmail.com>
2024-04-11 22:15:03 -0700

    adding packaged functions for preprocessing and simplifying script to address feedback

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,8 +2,8 @@
 #date: 2024-04-09
 
 """
-Preprocesses training and testing datasets by applying standard scaling to numeric features, 
-one-hot encoding to categorical features, and preserving binary and passthrough features. 
+Preprocesses training and testing datasets by applying standard scaling to numeric features and 
+one-hot encoding to categorical features. 
 Saves the preprocessed datasets to specified output files.
 
 Usage:
@@ -20,8 +20,9 @@ Arguments:
 
 import click
 import pandas as pd
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.compose import ColumnTransformer
+from py_predpurchase.function_preprocessing import numerical_categorical_preprocess
+# from sklearn.preprocessing import StandardScaler, OneHotEncoder
+# from sklearn.compose import ColumnTransformer
 
 @click.command()
 @click.argument('x_train_file', type=click.Path(exists=True))
@@ -32,17 +33,14 @@ from sklearn.compose import ColumnTransformer
 @click.argument('test_output_file', type=str)
 def preprocess_data(x_train_file, x_test_file, y_train_file, y_test_file, train_output_file, test_output_file):
     """
-    Reads the input dataset, applies preprocessing transformations, and saves
+    Reads the input dataset, applies numeric and categorical feature transformations, and saves
     the preprocessed data to an output file.
     """
     # Load the datasets
     X_train = pd.read_csv(x_train_file)
     X_test = pd.read_csv(x_test_file)
-
-    # Define the transformers
-    numeric_transformer = StandardScaler()
-    categorical_transformer = OneHotEncoder(handle_unknown="ignore", sparse_output=False)
-    binary_transformer = OneHotEncoder(drop='if_binary', dtype=int)
+    y_train = pd.read_csv(y_train_file)
+    y_test = pd.read_csv(y_test_file)
 
     # Define feature groups
     numeric_features = ['Administrative', 'Administrative_Duration', 'Informational',
@@ -51,45 +49,10 @@ def preprocess_data(x_train_file, x_test_file, y_train_file, y_test_file, train_
 
     categorical_features = ['Month', 'VisitorType']
 
-    passthrough_features = ['Browser', 'Region', 'TrafficType', 'OperatingSystems']
-
-    binary_features = ['Weekend']
-
-    # Create the preprocessor
-    # preprocessor = make_column_transformer(
-    #     (numeric_transformer, numeric_features),
-    #     (categorical_transformer, categorical_features),
-    #     (binary_transformer, binary_features),
-    #     ("passthrough", passthrough_features),
-    #     ("drop", drop_features),
-    # )
-
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('numeric', numeric_transformer, numeric_features),
-            ('categorical', categorical_transformer, categorical_features),
-            ('binary', binary_transformer, binary_features),
-            ('passthrough', 'passthrough', passthrough_features),
-            ]
+    # Use function from py_predpurchase package 
+    train_transformed, test_transformed, transformed_column_names = numerical_categorical_preprocess(
+        X_train, X_test, y_train, y_test, numeric_features, categorical_features
     )
-
-    # fitting and transforming data
-    X_train_transformed = preprocessor.fit_transform(X_train)
-
-    X_test_transformed = preprocessor.transform(X_test)
-
-    y_train = pd.read_csv(y_train_file)
-    y_test = pd.read_csv(y_test_file)
-
-    # obtaining transformed column names
-    categorical_columns = preprocessor.named_transformers_['categorical'].get_feature_names_out(categorical_features)
-    transformed_column_names = numeric_features + list(categorical_columns) + binary_features + passthrough_features
-
-    X_train_transformed_df = pd.DataFrame(X_train_transformed, columns=transformed_column_names)
-    X_test_transformed_df = pd.DataFrame(X_test_transformed, columns=transformed_column_names)
-
-    train_transformed = pd.concat([X_train_transformed_df, y_train], axis=1)
-    test_transformed = pd.concat([X_test_transformed_df, y_test], axis=1)
 
     # Save the preprocessed data to an output file
     train_transformed.to_csv(train_output_file, index=False)
