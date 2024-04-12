86c2a97606b57fe6c82e8a1082924878d6c49d4f
Nour Shawky <nourshawky03@hotmail.com>
2024-04-11 23:51:02 -0700

    deleting functions and test folder

--- tests/make_test_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-def create_dummy_data():
-    data = {
-        'feature1': [1, 2, 3, 4, 5],
-        'feature2': [5, 4, 3, 2, 1],
-        'target': [0, 1, 0, 1, 0]
-    }
-    df = pd.DataFrame(data)
-    df.to_csv('data/cross_val_test_data.csv', index=False)
